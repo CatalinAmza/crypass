74defefd62fd4752471516ddb6450ef5393f7d62
CatalinAmza <catalin.amza@depinfonancy.net>
2015-08-10 15:36:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from Log import Log
 import os
 import shutil
 import pyperclip
+import hashlib
+import argparse
+import getpass
+import sha3
+
 
 def generate(choice, infos, length, log):
     my_chars = []
@@ -46,7 +51,7 @@ def contains(l1, l2):
         res = False
         for b in l1:
             if a in b:
-                res = True
+                  res = True
         g_res &= res
     return g_res
 
@@ -97,66 +102,81 @@ def check_choice(text):
             print("Invalid choice, try again")
             return check_choice(input("1. Add new account\n2. Get an account\n3. Change a password\n4. Remove an account\n5. Change the master key\n6. Print a decrypted temp\nYour choice: "))
 
-if len(sys.argv) != 3:
-    print("Use this with the full path of your password file and your key.")
-    time.sleep(3)
-else:
-    key = sys.argv[2].encode()
-    if len(key) != 32:
-        print("Wrong key.")
-        time.sleep(3)
+def get_path():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', nargs=1)
+    args = parser.parse_args()
+    if args is None or not os.path.isfile(args.path[0]):
+            print("Path wrong or not provided.")
+            time.sleep(3)
+            sys.exit(0)
     else:
-        file = sys.argv[1]
-        now = time.time()
-        working_file = get_folder(sys.argv[0]) + get_file_name(file) + '.' + str(now).replace(".", "")
-        shutil.copyfile(file, working_file)
-        decrypt_file(working_file, key)
-        try:
-            data = Log(file_name=working_file, separator="  ||  ")
-            if len(data.log[0]) == 1:
-                raise Exception
-        except Exception:
-            print("Wrong key.")
-            time.sleep(3)
-        else:
-            choice = check_choice(input("1. Add new account\n2. Get an account\n3. Change a password\n4. Remove an account\n5. Change the master key\n6. Print a decrypted temp\nYour choice: "))
-            if choice == 1:
-                infos = []
-                for head in data.header:
-                    if "password" not in head.lower():
-                        infos.append(input(head + ": "))
+        return args.path[0]
+
+
+def run(file):
+    s = hashlib.new("sha3_256")
+    s.update(getpass.getpass().encode())
+    key = s.digest()
+    working_file = get_folder(sys.argv[0]) + get_file_name(file) + '.' + str(time.time()).replace(".", "")
+    shutil.copyfile(file, working_file)
+    decrypt_file(working_file, key)
+    try:
+        data = Log(file_name=working_file, separator="  ||  ")
+        if len(data.log[0]) == 1:
+            raise Exception
+    except Exception:
+        print("Try again.")
+        time.sleep(3)
+        return False
+    else:
+        os.remove(working_file)
+        choice = check_choice(input("1. Add new account\n2. Get an account\n3. Change a password\n4. Remove an account\n5. Change the master key\n6. Print a decrypted temp\nYour choice: "))
+        if choice == 1:
+            infos = []
+            for head in data.header:
+                if "password" not in head.lower():
+                    infos.append(input(head + ": "))
+            length = int(input("Password length: "))
+            generate(getType(), infos, length, data)
+        elif choice in [2,3,4]:
+            for i in range(len(data.header)):
+                if "password" in data.header[i].lower():
+                    pos = i
+            index = search(data)
+            if choice == 2:
+                pyperclip.copy(str(data.log[index][pos]))
+                temp = data.log[index][:]
+                temp.remove(temp[pos])
+                print("Here's your account: %s. \nCopied password to clipboard." % str(temp))
+            elif choice == 3:
                 length = int(input("Password length: "))
-                generate(getType(), infos, length, data)
-            elif choice in [2,3,4]:
-                for i in range(len(data.header)):
-                    if "password" in data.header[i].lower():
-                        pos = i
-                index = search(data)
-                if choice == 2:
-                    pyperclip.copy(str(data.log[index][pos]))
-                    data.log[index].remove(data.log[index][pos])
-                    print("Here's your account: %s. \nCopied password to clipboard." % str(data.log[index]))
-                elif choice == 3:
-                    length = int(input("Password length: "))
-                    x = data.log[index]
-                    x.remove(x[pos])
-                    data.log.remove(data.log[index])
-                    generate(getType(), x, length, data)
-                    print("Password successfully changed. Copied new password to clipboard.")
-                else:
-                    x = data.log.pop(index)
-                    data.save()
-                    print("Removed the following account: %s. Enjoy." % str(x))
-            elif choice == 5:
-                key = ""
-                while len(key) != 32:
-                    key = input("Insert your new key (32 characters long): ")
-                key = key.encode()
-                print("Successfully changed your key.")
+                x = data.log[index]
+                x.remove(x[pos])
+                data.log.remove(data.log[index])
+                generate(getType(), x, length, data)
+                print("Password successfully changed. Copied new password to clipboard.")
             else:
-                open(os.path.expanduser("~/Desktop/" + 'temp'), 'w').write(data.__str__())
-            print("Program terminated. Waiting 3 seconds before deleting the decrypted file.")
-            time.sleep(3)
-            shutil.copyfile(working_file, file)
-            os.remove(working_file)
-            encrypt_file(file, key)
+                x = data.log.pop(index)
+                x.remove(x[pos])
+                print("Removed the following account: %s. Enjoy." % str(x))
+        elif choice == 5:
+            z = hashlib.new("sha3_256")
+            z.update(getpass.getpass().encode())
+            key = z.digest()
+            print("Successfully changed your key.")
+        else:
+            open(os.path.expanduser("~/Desktop/" + 'temp'), 'w').write(data.__str__())
+        print("Program terminated.")
+        time.sleep(3)
+        data.save()
+        shutil.copyfile(working_file, file)
+        os.remove(working_file)
+        encrypt_file(file, key)
+        return True
+
+
+file = get_path()
+
+while not run(file):
+    ()
