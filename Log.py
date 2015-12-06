c9fc8420aad9f1b1d4444f38316740758fa2341e
CatalinAmza <catalin.amza@depinfonancy.net>
2015-12-06 14:15:26 +0100

    Update Log.py

--- Log.py
+++ Log.py
@@ -1,5 +1,5 @@
-class Log(): # no error detection, use carefully
-    def __init__(self, file_name='temp', separator=-1): # separator = -1 for one-item lines
+class Log(): 
+    def __init__(self, file_name='temp', separator=-1):
         self.file_name = file_name
         self.separator = separator
         self.header = []
